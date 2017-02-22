7762db007098f690b05b166132ff3c26fabf1105
Marvin Frederickson <marvin.frederickson@gmail.com>
2017-02-22 09:31:05 -0900

    use correct bundler path

--- config/deploy.rb
+++ config/deploy.rb
@@ -73,6 +73,9 @@ set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
+set :bundle_path, -> { shared_path.join('vendor/bundle') }
+
+
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
